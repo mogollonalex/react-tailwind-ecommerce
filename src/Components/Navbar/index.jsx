9faeb46b55b6ef3c713e1739bae5e91594d45195
Alex <mogollonwilmer@gmail.com>
2023-05-10 20:50:05 -0400

    correction

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
           <NavLink to='/'>
-            Shopi
+            Pytronic, C.A.
           </NavLink>
         </li>
         <li>
